d207589c2982bb741af73703fd40686e340d8de6
lai2-24 <lai2-24@itismeucci.com>
2025-03-12 10:38:24 +0100

    CRUD su Alunni (da ultimare)

--- controllers/AlunniController.php
+++ controllers/AlunniController.php
@@ -12,4 +12,38 @@ class AlunniController
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+
+  public function getOne(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id=$id");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function createOne(Request $request, Response $response, $args){
+    $body = json_decode($request->getBody());
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $query = "INSERT INTO alunni (nome, cognome) VALUES ('$body->nome', '$body->cognome');";
+    $mysqli_connection->query($query) or die ('Unable to execute query. '. mysqli_error($query));
+    return $response->withStatus(200);
+  }
+
+  public function updateOne(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $body = json_decode($request->getBody());
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $query = "UPDATE alunni SET nome = '$body->nome', cognome= '$body->cognome' WHERE id = $id;";
+    $mysqli_connection->query($query) or die ('Unable to execute query. '. mysqli_error($query));
+    return $response->withStatus(200);
+  }
+
+  public function deleteOne(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $mysqli_connection->query("DELETE FROM alunni WHERE id='$id';") or die ('Unable to execute query. '. mysqli_error($query));
+    return $response->withStatus(200);
+  }
 }
--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/controllers/HelloController.php';
+require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
 
-$app->get('/hello', "HelloController:hello");
-$app->get('/hello/{name}', "HelloController:hello_with_name");
-$app->get('/json/{name}', "HelloController:json_name");
+$app->get('/alunni', "AlunniController:index");
+$app->get("/alunni/{id}", "AlunniController:getOne");
+$app->post("/alunni", "AlunniController:createOne");
+$app->put("/alunni/{id}", "AlunniController:updateOne");
+$app->delete("/alunni/{id}", "AlunniController:deleteOne");
 
 $app->run();
 
