97ad468c6b88f5ba681f4285bea8a595a6b1fe6a
lai2-24 <lai2-24@itismeucci.com>
2025-03-26 08:46:03 +0100

    aggiunta dei file mancanti

--- php/index.php
+++ php/index.php
@@ -7,5 +7,11 @@ require __DIR__ . '/controllers/AlunniController.php';
 $app = AppFactory::create();
 
 $app->get('/alunni', "AlunniController:index");
+$app->get("/alunni/{id}", "AlunniController:getOne");
+$app->post("/alunni", "AlunniController:createOne");
+$app->put("/alunni/{id}", "AlunniController:updateOne");
+$app->delete("/alunni/{id}", "AlunniController:deleteOne");
 
 $app->run();
+
+?>
