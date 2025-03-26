bc80f8ed5601b80e70f159db874bf1349af61ae5
lai2-24 <lai2-24@itismeucci.com>
2025-03-26 09:16:50 +0100

    aggiornamento cartelle ed esercizio ultimato

--- php/index.php
+++ php/index.php
@@ -6,10 +6,17 @@ require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
 
+// Per visualizzare basta aprire chrome
 $app->get('/alunni', "AlunniController:index");
 $app->get("/alunni/{id}", "AlunniController:getOne");
+
+// Per creare un nuovo alunno: curl -X POST localhost:8080/alunni -d '{"nome":"aaa","cognome":"bbb"}' -H "Content-Type: application/json"
 $app->post("/alunni", "AlunniController:createOne");
+
+// Per aggiornare un alunno esistente: curl -X PUT localhost:8080/alunni/3 -d '{"nome":"Stefano","cognome":"il meglio"}' -H "Content-Type: application/json"
 $app->put("/alunni/{id}", "AlunniController:updateOne");
+
+// Per eliminare un alunno esistente: curl -X DELETE localhost:8080/alunni/3 
 $app->delete("/alunni/{id}", "AlunniController:deleteOne");
 
 $app->run();
